d8ea66c5bbc9726c975d5a5827359ca4ae9df98d
S-Kaito <irc.darjeeling@gmail.com>
2018-11-02 19:43:42 +0900

    2018_1102 19:43

--- Hello-world/DeepLearning/Sample01.py
+++ Hello-world/DeepLearning/Sample01.py
@@ -1,9 +1,8 @@
-%matplotlib inline
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-INPUT_DATA = (np.arange(0,np.pi*2,0.1)-np.pi)/np.pi
+INPUT_DATA = (np.arange(0 ,np.pi*2 ,0.1)-np.pi)/np.pi
 WEIGHT = 0.1
 
 class Layer:
@@ -18,15 +17,15 @@ class MiddleLayer(Layer):
     def forward(this,x):
         super().forward(x)
         this.y = 1/(1+(np.exp(-this.y)))
-        
+
 layer = MiddleLayer(1,3)
 output = Layer(3,1)
 
-layer.w = np.array([[ 1.5226207,-4.74313941,-5.09765436]])
-layer.b = np.array([ 0.05025657,0.25530377,-1.12453411])
+layer.w = np.array([[ 5,2,-5]])
+layer.b = np.array([ 0.1,0.2,-1])
 
-output.w = np.array([9.01367277,5.04463465,1.2634446 ])
-output.b = np.array([-7.73053153])
+output.w = np.array([9,3,4])
+output.b = np.array([-5])
 
 plot_x = []
 plot_y = []
@@ -34,9 +33,11 @@ plot_y = []
 for i in INPUT_DATA:
     layer.forward(np.array([i]).reshape(1,1))
     output.forward(layer.y)
-    
+
     plot_x.append(i)
     plot_y.append(output.y)
 
-plt.scatter(plot_x,plot_y,marker="+")
+print(layer.w)
+
+plt.scatter(plot_x, plot_y, marker="+")
 plt.show()
