825fa0c4671f3ca8a28d8243dfc9d51fa1fe3ad6
S-Kaito <irc.darjeeling@gmail.com>
2018-11-05 23:22:37 +0900

    2018_1105 23:22

--- Hello-world/DeepLearning/Sample02.py
+++ Hello-world/DeepLearning/Sample02.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 INPUT_DATA = np.linspace(-1.0,1.0,100)
-OUTPUT_DATA = np.sin(INPUT_DATA)
+OUTPUT_DATA = np.cos(INPUT_DATA * np.pi)
 WEIGHT = 0.1
+EPOCH = 2000
 
 class Layer:
     def __init__(this,input_num,output_num):
@@ -17,25 +18,38 @@ class Layer:
         this.y = np.dot(x,this.w) + this.b
 
     def backward(this,x):
-        this.bx = x
-        this.by = np.dot(x,this.w.T)
+        delta = this.y - x
+
+        this.grad_w = np.dot(this.x.T,delta)
+        this.grad_b = np.sum(delta,axis=0)
+
+        this.grad_x = np.dot(delta,this.w.T)
 
     def update(this):
-        this.w -= (this.w - this.by.T) * WEIGHT
-        this.b -= (np.sum(this.y) - np.sum(this.by)) * WEIGHT
+        this.w -= this.grad_w * WEIGHT
+        this.b -= this.grad_b * WEIGHT
 
 class MiddleLayer(Layer):
     def forward(this,x):
         super().forward(x)
         this.y = 1/(1+(np.exp(-this.y)))
 
+    def backward(this,x):  
+        delta = x * (1 - this.y) * this.y
+
+        this.grad_w = np.dot(this.x.T,delta)
+        this.grad_b = np.sum(delta,axis=0)
+
+        this.grad_x = np.dot(delta,this.w.T)
+
 layer = MiddleLayer(1,3)
 output = Layer(3,1)
 
 fig = plt.figure()
-img = []
 
-for j in range(100):
+img = []
+    
+for j in range(EPOCH):
     plot_x = []
     plot_y = []
     for i in INPUT_DATA:
@@ -46,15 +60,17 @@ for j in range(100):
         plot_y.append(np.sum(output.y))
 
         output.backward(OUTPUT_DATA[np.where(INPUT_DATA == i)].reshape(1,1))
-        layer.backward(output.by)
+        layer.backward(output.grad_x)
 
         layer.update()
         output.update()
 
-    if j % 10 == 0:
-        print(j," / 100 ",layer.w,layer.b,output.w,output.b)
-    plt.plot(INPUT_DATA,OUTPUT_DATA,linestyle="dashed")
-    line, = plt.plot(plot_x,plot_y,"r")
-    img.append([line])
+    if j % (EPOCH / 20) == 0:
+        print(j + (EPOCH / 20)," / ",EPOCH)
+        print(layer.w,layer.b)
+        print(output.w,output.b)
+        plt.plot(INPUT_DATA,OUTPUT_DATA,linestyle="dashed")
+        line = plt.scatter(plot_x,plot_y,marker="+")
+        img.append(line)
 ani = animation.ArtistAnimation(fig,img)
 plt.show()
