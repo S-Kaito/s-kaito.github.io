7c42f543566b573b01a662b1000a7249781793dc
S-Kaito <irc.darjeeling@gmail.com>
2018-11-05 18:46:45 +0900

    2018_1105 18:46

--- Hello-world/DeepLearning/Sample02.py
+++ Hello-world/DeepLearning/Sample02.py
@@ -1,103 +1,60 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-input_data = np.arange(0,np.pi*2,0.1)
-correct_data = np.sin(input_data)
-input_data = (input_data - np.pi)/np.pi
-n_data = len(correct_data)
+INPUT_DATA = np.linspace(-1.0,1.0,100)
+OUTPUT_DATA = np.sin(INPUT_DATA)
+WEIGHT = 0.1
 
-n_in = 1
-n_mid = 3
-n_out = 1
+class Layer:
+    def __init__(this,input_num,output_num):
+        this.w = WEIGHT * np.random.randn(input_num,output_num)
+        this.b = WEIGHT * np.random.randn(output_num)
+        
+    def forward(this,x):
+        this.x = x
+        this.y = np.dot(x,this.w) + this.b
 
-wb_width = 0.01
-eta = 0.1
-epoch = 3001
-interval = 100
+    def backward(this,x):
+        this.bx = x
+        this.by = np.dot(x,this.w.T)
 
-class MiddleLayer:
-    def __init__(self,n_upper , n):
-        self.w = wb_width * np.random.randn(n_upper, n)
-        self.b = wb_width * np.random.randn(n)
-        
-    def forward(self ,x):
-        self.x = x
-        u = np.dot(x,self.w)  + self.b
-        self.y = 1/(1 + np.exp(-u))
+    def update(this):
+        this.w -= (this.w - this.by.T) * WEIGHT
+        this.b -= (np.sum(this.y) - np.sum(this.by)) * WEIGHT
 
-    def backward(self,grad_y):
-        delta = grad_y * (1-self.y) * self.y
-        self.grad_w = np.dot(self.x.T, delta)
-        self.grad_b = np.sum(delta, axis=0)
-        self.grad_x = np.dot(delta,self.w.T)
-        
-    def update(self,eta):
-        self.w -= eta * self.grad_w
-        self.b -= eta * self.grad_b
-    
-class OutputLayer:
-    def __init__(self, n_upper , n):
-        self.w = wb_width * np.random.randn(n_upper, n)
-        self.b = wb_width * np.random.randn(n)
-        
-    def forward(self,x):
-        self.x = x
-        u = np.dot(x,self.w) + self.b
-        self.y = u
-        
-    def backward(self,t):
-        delta = self.y - t
-        self.grad_w = np.dot(self.x.T , delta)
-        self.grad_b = np.sum(delta, axis=0)
-        
-        self.grad_x = np.dot(delta, self.w.T)
-        
-    def update(self, eta):
-        self.w -= eta * self.grad_w
-        self.b -= eta * self.grad_b
-        
-middle_layer = MiddleLayer(n_in,n_mid)
-output_layer = OutputLayer(n_mid, n_out)
+class MiddleLayer(Layer):
+    def forward(this,x):
+        super().forward(x)
+        this.y = 1/(1+(np.exp(-this.y)))
+
+layer = MiddleLayer(1,3)
+output = Layer(3,1)
 
 fig = plt.figure()
+img = []
 
-def plot(i):
-    index_random = np.arange(n_data)
-    np.random.shuffle(index_random)
-    
-    total_error = 0
+for j in range(100):
     plot_x = []
     plot_y = []
-    
-    for idx in index_random:
-        x = input_data[idx:idx + 1]
-        t = correct_data[idx:idx + 1]
+    for i in INPUT_DATA:
+        layer.forward(np.array([i]).reshape(1,1))
+        output.forward(layer.y)
 
-        middle_layer.forward(x.reshape(1,1))
-        output_layer.forward(middle_layer.y)
+        plot_x.append(i)
+        plot_y.append(np.sum(output.y))
 
-        output_layer.backward(t.reshape(1,1))
-        middle_layer.backward(output_layer.grad_x)
+        output.backward(OUTPUT_DATA[np.where(INPUT_DATA == i)].reshape(1,1))
+        layer.backward(output.by)
 
-        middle_layer.update(eta)
-        output_layer.update(eta)
+        layer.update()
+        output.update()
 
-        if i % interval == 0:
-            y = output_layer.y.reshape(-1)
-
-            total_error += 1.0/2.0*np.sum(np.square(y-t))
-
-            plot_x.append(x)
-            plot_y.append(y)
-
-            plt.cla()
-            plt.plot(input_data, correct_data, linestyle="dashed")
-            plt.scatter(plot_x,plot_y,marker="+")
-        if i % (interval * 10) == 0:
-            print(middle_layer.w,middle_layer.b)
-            print(output_layer.w,output_layer.b)
-            print()
-
-ani = animation.FuncAnimation(fig, plot, interval=1)
+    if j % 10 == 0:
+        print(j," / 100 ",layer.w,layer.b,output.w,output.b)
+    plt.plot(INPUT_DATA,OUTPUT_DATA,linestyle="dashed")
+    line, = plt.plot(plot_x,plot_y,"r")
+    img.append([line])
+ani = animation.ArtistAnimation(fig,img)
 plt.show()
