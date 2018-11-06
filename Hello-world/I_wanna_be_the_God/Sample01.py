4f605193237388bd17bf6843593c501e6a2cb50e
S-Kaito <irc.darjeeling@gmail.com>
2018-11-06 19:05:39 +0900

    2018_1106 19:05

--- Hello-world/I_wanna_be_the_God/Sample01.py
+++ Hello-world/I_wanna_be_the_God/Sample01.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 WEIGHT = 0.1
-EPOCH = 2000
-MAP = np.random.randint(0,100,(110,110))
-MAP[0:99][0] = 0
-MAP[0:99][98]= 0
-MAP[0][0:99] = 0
-MAP[99][0:99]= 0
+EPOCH = 1000
+MAP = np.random.randint(0,100,(100,100))
+ANS = np.sum(MAP)
+MAP[0:5,0:99] = -5
+MAP[95:99,0:99] = -5
+MAP[0:99,0:5] = -5
+MAP[0:99,95:99] = -5
 
 class Layer:
     def __init__(this,input_num,output_num):
@@ -55,36 +56,61 @@ brain.append(Layer(10,5))
 x = 50
 y = 50
 point = 0
-action = []
+action = [0,1,2,3,4]
+memory = [np.zeros(25) * 5]
 plot_x = []
 plot_y = []
 for j in range(EPOCH):
-    print(np.array(MAP[x - 2::x + 2][y - 2::y + 2]).shape)
-    brain[0].forward(np.array(MAP[x - 2:x + 2][y - 2:y + 2]).reshape(1,-1))
+    memory.append(np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25))
+    brain[0].forward(np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25))
     for i in range(1,5):
         brain[i].forward(brain[i - 1].y)
-    action.append(np.where(brain[4].y == np.amax(brain[4].y)))
-    if action == 0:
-        y -= 1
-    elif action == 1:
-        y += 1
-    elif action == 2:
-        x += 1
-    elif action == 3:
-        x -= 1
-    elif action == 4:
-        point += MAP[x][y]
-        brain[4].backward([[0,0,0,0,MAP[x][y] / 10]])
-        for i in range(3,-1,-1):
-            brain[i].backword(brain[i - 1].grad_x)
+    action.append(np.argmax(brain[4].y))
 
-        for i in brain:
-            i.update()
+    args = np.zeros(5)
+
+    if action[-1] == 0:
+        y -= 1
+    elif action[-1] == 1:
+        y += 1
+    elif action[-1] == 2:
+        x += 1
+    elif action[-1] == 3:
+        x -= 1
+    else:
+        point += MAP[x][y]
+        args[4] += MAP[x][y] / 10
+        a = np.zeros(5)
+        a[action[-2]] += MAP[x][y] / 15
+        args[action[-3]] += MAP[x][y] / 20
+        MAP[x][y] = 0
+
+    args[action[-1]] -= 0.5
+    
+    brain[4].backward(args)
+    for i in range(3,-1,-1):
+        brain[i].backward(brain[i + 1].grad_x)
+
+    for i in brain:
+        i.update()
+
 
     if j % (EPOCH / 20) == 0:
-        print(j + (EPOCH / 20)," / ",EPOCH)
+        print()
+        print(j + (EPOCH / 20)," / ",EPOCH,"  POINT:",point)
+        print(MAP[x - 2:x + 3,y - 2:y + 3])
+        print(brain[4].b)
         plot_x.append(j)
         plot_y.append(point)
+        np.savetxt("map.txt",MAP)
 
+    MAP[0:5,0:99] = -5
+    MAP[95:99,0:99] = -5
+    MAP[0:99,0:5] = -5
+    MAP[0:99,95:99] = -5
+    MAP[6:94,6:94] += 1
+    MAP[MAP > 99] = 99
+
+print(ANS - np.sum(MAP),"/",ANS)
 plt.scatter(plot_x,plot_y,marker="+")
 plt.show()
