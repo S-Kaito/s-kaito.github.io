25025c21f111e37a521b0ed7caf812edaa9bff5b
S-Kaito <irc.darjeeling@gmail.com>
2018-11-08 01:18:57 +0900

    2018_1108 01:18

--- Hello-world/I_wanna_be_the_God/Sample01.py
+++ Hello-world/I_wanna_be_the_God/Sample01.py
@@ -5,13 +5,9 @@ import matplotlib.animation as animation
 
 WEIGHT = 0.01
 ETA = 0.1
-EPOCH = 10000
-MAP = np.random.randint(0,20,(1000,1000))
+EPOCH = 5000
+MAP = np.random.randint(-10,20,(100,100))
 ANS = np.sum(MAP[5:-5,5:-5])
-MAP[0:5,0:-5] = -99
-MAP[-10:-5,0:-5] = -99
-MAP[0:-5,0:5] = -99
-MAP[0:-5,-10:-5] = -99
 
 class Layer:
     def __init__(this,input_num,output_num):
@@ -72,7 +68,7 @@ class LayerSoftmax(Layer):
 class Creat:
     def __init__(this,brain):
         this.brain = brain
-        this.map = [np.zeros((1,25)),np.zeros((1,25)),np.zeros((1,25)),np.zeros((1,25))]
+        this.map = [np.zeros((1,49)),np.zeros((1,49)),np.zeros((1,49)),np.zeros((1,49))]
         this.action = [0,1,2,3]
 
     def forward(this,x):
@@ -97,7 +93,7 @@ class Creat:
     def learn(this,point):
         for i in range(1,4):
             a = np.zeros(5)
-            a[this.action[-i]] += point / ((i) * 5)
+            a[this.action[-i]] += point / ((i))
             this.forward(this.map[-i])
             this.backward(a)
             this.update()
@@ -107,8 +103,13 @@ class Creat:
             np.savetxt('w0' + str(i) + '.csv', this.brain[i].w, delimiter=',')
             np.savetxt('b0' + str(i) + '.csv', this.brain[i].b, delimiter=',')
 
-creat = Creat([LayerSigmoid(25,10),LayerSoftmax(10,5)])
+    def load(this):
+        for i in range(0,len(this.brain)):
+            this.brain[i].w = np.loadtxt('w0' + str(i) + '.csv', delimiter=',')
+            this.brain[i].b = np.loadtxt('b0' + str(i) + '.csv', delimiter=',')
 
+creat = Creat([LayerSigmoid(49,50),LayerSigmoid(50,50),LayerSoftmax(50,5)])
+creat.load()
 x = 50
 y = 50
 point = 0
@@ -117,11 +118,11 @@ plot_y = []
 
 for j in range(EPOCH):
     #print(MAP[x - 2:x + 3,y - 2:y + 3],creat.action[-1])
-    creat.forward(np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25))
-    creat.memory(np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25),np.random.choice(np.array([0,1,2,3,4]), 1, p= (np.exp(creat.brain[-1].y)/np.sum(np.exp(creat.brain[-1].y))).reshape(-1)))
+    creat.forward(np.array(MAP[x - 3:x + 4,y - 3:y + 4]).reshape(1,49)/20)
+    creat.memory(np.array(MAP[x - 3:x + 4,y - 3:y + 4]).reshape(1,49)/20,np.random.choice(np.array([0,1,2,3,4]), 1, p= (np.exp(creat.brain[-1].y)/np.sum(np.exp(creat.brain[-1].y))).reshape(-1)))
 
     args = np.zeros(5)
-    args[creat.action[-1]] -= 0.05
+    args[creat.action[-1]] -= 0.01
     creat.backward(args)
     creat.update()
     if creat.action[-1] == 0:
@@ -134,7 +135,7 @@ for j in range(EPOCH):
         x -= 1
     elif creat.action[-1] == 4:
         point += MAP[x][y]
-        creat.learn(MAP[x][y])
+        creat.learn(MAP[x][y]/20)
         MAP[x][y] = 0
 
     if j % (EPOCH / 100) == 0:
@@ -143,16 +144,17 @@ for j in range(EPOCH):
     if j % (EPOCH / 50) == 0:
         print()
         print(j," / ",EPOCH,"  POINT:",point,"/",ANS)
-        print(MAP[x - 2:x + 3,y - 2:y + 3])
+        print(np.array(MAP[x - 3:x + 4,y - 3:y + 4]))
         #print(creat.brain[-1].y,np.argmax(creat.brain[-1].y))
         
-    MAP[0:5,0:-5] = -20
-    MAP[-10:-5,0:-5] = -20
-    MAP[0:-5,0:5] = -20
-    MAP[0:-5,-10:-5] = -20
+    MAP[0:5,:] = -20
+    MAP[-5:,:] = -20
+    MAP[:,0:5] = -20
+    MAP[:,-5:] = -20
     MAP[MAP > 20] = 20
 
 print(np.sum(MAP[5:95,5:95]),"/",ANS)
+print(MAP)
 creat.save()
 plt.scatter(plot_x,plot_y,marker="+")
 plt.show()
