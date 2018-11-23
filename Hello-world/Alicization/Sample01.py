5c94cbf2a87ab62f10177d98fc57a542ef7d5f98
S-Kaito <irc.darjeeling@gmail.com>
2018-11-23 19:23:56 +0900

    2018_1123 19:23

--- Hello-world/Alicization/Sample01.py
+++ Hello-world/Alicization/Sample01.py
@@ -5,6 +5,8 @@ import matplotlib.animation as animation
 import sys
 import tensorflow as tf
 
+from statistics import mean, median,variance,stdev
+
 import alice as ac
 
 EPOCH = 10000
@@ -12,51 +14,59 @@ EPOCH = 10000
 def run(creat):
 
 	MAP = np.random.randint(-19,20,(100,100))
-	ANS = np.sum(MAP)
+	ANS = np.sum(MAP[MAP > 0])
 
 	x = 50
 	y = 50
 	point = 0
+
 	plot_x = []
 	plot_y = []
 	death = []
+	error = [0]
 	deathCount = 0
 
-	for j in range(EPOCH):
+	for j in range(1,EPOCH):
 		creat.forward(np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25))
 
-		creat.memory(np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25),np.random.choice(np.array([0,1,2,3]), 1, p=np.exp(creat.evaluation)/np.sum(np.exp(creat.evaluation)).reshape(-1)))
-
+		creat.memory(map=np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25),action=np.random.choice([0,1,2,3], 1, p=np.exp(creat.network.y.reshape(-1))/np.sum(np.exp(creat.network.y.reshape(-1))).reshape(-1)))
+		#creat.memory(map=np.array(MAP[x - 2:x + 3,y - 2:y + 3]).reshape(1,25),action=np.argmax(creat.network.y))
 		
-		if creat.action[-1] == 0:
+		if creat.action[-1][0] == 0:
 			y -= 1
-		elif creat.action[-1] == 1:
+		elif creat.action[-1][0] == 1:
 			y += 1
-		elif creat.action[-1] == 2:
+		elif creat.action[-1][0] == 2:
 			x += 1
-		elif creat.action[-1] == 3:
+		elif creat.action[-1][0] == 3:
 			x -= 1
+
 		point += MAP[x][y]
 
-		if j > 5:
-			creat.learn(MAP[x][y])
+		error[-1] += (ac.clipping(MAP[x][y]) - ac.clipping(creat.network.y[0][creat.action[-1][0]])) ** 2
+
+		creat.memory(point=MAP[x][y])
+
 		MAP[x][y] = 0
 
-		if (x == 8 or x == 91) or (y == 8 or y == 91):
-			creat.learn(MAP[x][y])
+		if (x == 9 or x == 90) or (y == 9 or y == 90):
 			x = 50
 			y = 50
 			deathCount += 1
 			
 		if j % (EPOCH / 100) == 0:
+
+			error[-1] *= 10
+
 			plot_x.append(j)
 			plot_y.append(point)
 			death.append(deathCount * 100)
+			error.append(0)
+
 		if j % (EPOCH / 20) == 0:
 			print()
 			print(j," / ",EPOCH,"  POINT:",point,"/",ANS," DEATH:",deathCount)
 			print(np.array(MAP[x - 3:x + 4,y - 3:y + 4]))
-			#print(creat.brain[-1].y,np.argmax(creat.brain[-1].y))
 			
 		MAP[0:10,:] = -20
 		MAP[-10:,:] = -20
@@ -64,20 +74,45 @@ def run(creat):
 		MAP[:,-10:] = -20
 		MAP[MAP > 20] = 20
 
-	print(np.sum(MAP),"/",ANS)
-	print(MAP)
+	print(np.sum(MAP[MAP > 0]),"/",ANS)
+	print(MAP[10:-10,10:-10])
+	plt.cla()
 	plt.scatter(plot_x,plot_y,marker="+")
 	plt.scatter(plot_x,death,marker="*")
-	plt.show()
+	plt.scatter(plot_x,error[0:-1],marker="o")
+	
+	creat.learn()
+
+	return plt,(plot_y[-1],death[-1],mean(error))
 
 def main(args):
-	creat = ac.Creat(ac.Network([ac.LayerSigmoid(29,100),ac.LayerSigmoid(100,100),ac.LayerIdentity(100,1)]))
-	if "-r" in args:
-		creat.load()
-	
-	run(creat)
+	N = 1 if ("-t" not in args) else int(args[int(args.index('-t')) + 1])
+	plot_x= []
+	point = []
+	death = []
+	error = []
+	for i in range(N):
+		creat = ac.Creat(ac.Network([ac.LayerSigmoid(25,100),ac.LayerSigmoid(100,100),ac.LayerSigmoid(100,50),ac.LayerIdentity(50,4)]))
+		if "-r" in args:
+			creat.load()
+		
+		plt,result = run(creat)
+		plot_x.append(i + 1)
+		point.append(result[0])
+		death.append(result[1])
+		error.append(result[2])
+		if "-show-all" in args or i == N - 1:
+			plt.show()
 
-	if "-w" in args:
-		creat.save()
+		if "-w" in args:
+			creat.save()
+
+	plt.cla()
+	plt.scatter(plot_x,point,marker="+")
+	plt.scatter(plot_x,death,marker="*")
+	plt.scatter(plot_x,error,marker="o")
+	plt.title("Total Result")
+	plt.xlabel("Times")
+	plt.show()
 
 main(sys.argv)
--- Hello-world/Alicization/alice.py
+++ Hello-world/Alicization/alice.py
@@ -1,10 +1,14 @@
 import numpy as np
+import random
 import tensorflow as tf
 
 from abc import ABCMeta , abstractmethod
 
 WEIGHT = 0.1
-ETA = 0.1
+ETA = 0.01
+
+def clipping(point):
+	return 1 if point > 0 else (-1 if point < 0 else 0)
 
 class Layer(metaclass=ABCMeta):
 	def __init__(this,input_num,output_num):
@@ -126,20 +130,22 @@ class Network():
 class Creat:
 	def __init__(this,network):
 		this.network = network
-		this.map = [np.zeros((1,25)),np.zeros((1,25)),np.zeros((1,25)),np.zeros((1,25))]
-		this.action = [0,1,2,3]
+		this.map = []
+		this.action = []
+		this.point = []
 
 	def forward(this,x):
-		this.evaluation = []
-		for j in range(4):
-			a = np.zeros(4).reshape((1,4))
-			a[0][j] = 1
-			this.network.forward(np.hstack((x,a)).reshape((1,29)))
-			this.evaluation.append(np.sum(this.network.y))
+		# for j in range(4):
+		# 	a = np.zeros(4).reshape((1,4))
+		# 	a[0][j] = 1
+		# 	#this.network.forward(np.hstack((x,a)).reshape((1,29)))
+		# 	this.network.forward(x)
+		# 	this.evaluation.append(np.sum(this.network.y))
 		
-		a = np.zeros(4).reshape((1,4))
-		a[0][this.evaluation.index(max(this.evaluation))] = 1
-		this.network.forward(np.hstack((x,a)).reshape((1,29)))
+		# a = np.zeros(4).reshape((1,4))
+		# a[0][this.evaluation.index(max(this.evaluation))] = 1
+		# this.network.forward(np.hstack((x,a)).reshape((1,29)))
+		this.network.forward(x)
 
 		return this.network.y
 
@@ -149,24 +155,33 @@ class Creat:
 	def update(this):
 		this.network.update()
 
-	def memory(this,map,action):
-		this.map.append(map)
-		this.action.append(action)
+	def memory(this,map=None,action=None,point=None):
+		if map is not None:
+			this.map.append(map)
+		if action is not None:
+			this.action.append(action)
+		if point is not None:
+			this.point.append(point)
 
-	def learn(this,point):
-		for i in range(1,4):
+	def learn(this,turn=-1):
+		trainData = []
+
+		for i in range(-1,-(turn if turn != -1 else len(this.map)),-1):
+			trainData.append((this.map[i],this.action[i],this.point[i]))
+		random.shuffle(trainData)
+		for j in range(0,int(len(trainData) / 1.2)):
 			a = np.zeros(4).reshape((1,4))
-			a[0][this.action[-i]] = 1
-			this.network.forward(np.hstack((this.map[-i],a)).reshape((1,29)))
-			this.network.backward(point)
+			a[0][trainData[j][1]] = 1
+			#this.network.forward(np.hstack((trainData[j][0],a)).reshape((1,29)))
+			this.network.forward(trainData[j][0])
+			a[0][trainData[j][1]] = clipping(trainData[j][2])
+			this.network.backward(a)
 			this.network.update()
-
+		this.map = []
+		this.action = []
+		this.point = []
 	def save(this):
-		for i in range(0,len(this.network)):
-			np.savetxt('w0' + str(i) + '.csv', this.network[i].w, delimiter=',')
-			np.savetxt('b0' + str(i) + '.csv', this.network[i].b, delimiter=',')
+		this.network.save()
 
 	def load(this):
-		for i in range(0,len(this.network)):
-			this.network[i].w = np.loadtxt('w0' + str(i) + '.csv', delimiter=',')
-			this.network[i].b = np.loadtxt('b0' + str(i) + '.csv', delimiter=',')
+		this.network.load()
